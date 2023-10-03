57a9ed7333d8e2d77c76372f3d5028cfc8f2146f
AyoubEchcharrat <ayoub.ech-charrat@hotmail.fr>
2023-10-03 13:47:02 +0200

    animation on home page + font size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,39 @@
+"use client"
+
 require('dotenv').config()
 import styles from './page.module.css'
 import Image from 'next/image'
+import { useEffect,useState } from 'react'
 
 export default function Page() {
+  const [valeur, setValeur] = useState(30)
+  const valueY2 = 20
 
   function subline() {
     return "<subline> Exploration Subtile du Monde Web et de ses Langages.</subline>"
+  }
+
+  useEffect(() => {
+    const interval = setInterval(
+      () => setValeur(Math.floor(Math.random() * valueY2 + 30)),
+      1500
+    );
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const movingLineY2 = {
+      height: `${valeur}px`,
+      transition: "height .5s ease-in-out",
+      borderLeft: "dashed black 1px",
+      position: "absolute" as "absolute",
+      top: "220px",
+      left: "190px",
+      fontSize: "8px",
+      display: "flex",
+      alignItems: "center",
+      padding: "4px",
   }
 
   return (
@@ -13,7 +41,7 @@ export default function Page() {
       <div className={styles.firstcontenthome}>
         <div className={styles.lineanimationx}>190px</div>
         <div className={styles.lineanimationy1}>172px</div>
-        <div className={styles.lineanimationy2}>40px</div>
+        <div style={movingLineY2}>{movingLineY2.height}</div>
         <Image className={styles.catimagehome} src="/cat.png" width={467} height={441} alt="subtile draw of cat" />
         <p className={styles.headlinehome}>Cultivez votre curiosité.\n</p>
         <p className={styles.sublinehome}> {subline()} </p>
