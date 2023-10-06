8c5b777014136d1b2a91c15f28ed45c557b3006d
AyoubEchcharrat <ayoub.ech-charrat@hotmail.fr>
2023-10-06 18:29:47 +0200

    bug fix same

--- src/components/articles/Articles.tsx
+++ src/components/articles/Articles.tsx
@@ -2,33 +2,42 @@
 
 import axios from "axios"
 import Link from "next/link"
-
-async function getAllArticles() {
-    try{
-      const data = await axios.get(
-        `${process.env.PROD_URL}api/articles/`
-        )
-       return data.data
-    }
-    catch (error){
-      console.log({erreur : {error}})
-    }
-  }
+import { useEffect, useState } from "react";
 
 type unArticle = {
-    article:string,
-    title:string,
-    _id:string,
-    userId:string,
-    __v:number
+  article:string,
+  title:string,
+  _id:string,
+  userId:string,
+  __v:number
 }
 
-export default async function Articles() {
-    const data = await getAllArticles()
-    return (
+export default function Articles() {
+  const [data, setData] = useState([]); // Utilisation de useState pour gérer les données
+
+  useEffect(() => {
+    // Utilisation de useEffect pour effectuer la requête au chargement de la page
+    async function getAllArticles() {
+      try {
+        const response = await axios.get(
+          `${process.env.PROD_URL}api/articles/?timestamp=${Date.now()}`
+        );
+        setData(response.data); // Met à jour les données dans l'état
+      } catch (error) {
+        console.log({ erreur: { error } });
+      }
+    }
+
+    getAllArticles(); // Appel de la fonction pour récupérer les données
+  }, []); // Utilisation d'une dépendance vide pour que cela soit exécuté une seule fois
+
+  return (
       <div>
           <ul>
-            {data.map((article:unArticle,index:number) => <li key={index}><Link href={`articles/${article._id}`}>{article.title}</Link></li>)}
+            {data.map((article:unArticle,index:number) => 
+              <li key={index}>
+                <Link href={`articles/${article._id}`}>{article.title}</Link>
+              </li>)}
           </ul>
        
        </div>
