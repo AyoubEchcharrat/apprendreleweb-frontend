c5859a4d6fc9f1c6d24d7c3ea8dcc2bdd3a96f12
AyoubEchcharrat <ayoub.ech-charrat@hotmail.fr>
2023-11-30 15:56:20 +0100

    try to fix gtm

--- src/lib/gtm.ts
+++ src/lib/gtm.ts
@@ -5,7 +5,7 @@ type WindowWithDataLayer = Window & {
   
   declare const window: WindowWithDataLayer
   
-  export const GTM_ID = process.env.NEXT_PUBLIC_GTM
+  export const GTM_ID = "G-1D3SX2YGSG"
   
   export const pageview = (url: string) => {
     if (typeof window.dataLayer !== "undefined") {
