14db758cedf2d0c96d298fbb2cb85bd1e860e726
AyoubEchcharrat <ayoub.ech-charrat@hotmail.fr>
2024-02-11 17:27:55 +0100

    modify homepage content, modify styles articles, add content in about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,18 +1,36 @@
-import styles from "../page.module.css"
-import Section from '../../components/section/Section.jsx'
+import styles from "../page.module.css";
+import Section from "../../components/section/Section.jsx";
+import "./about.css";
 
 export const metadata = {
-    title: 'À propos | Apprendre Le Web',
-    description: "Explorez le monde du développement web avec des articles captivants et instructifs sur ApprendreLeWeb. Plongez dans des sujets variés, de la programmation aux dernières tendances, pour enrichir vos connaissances et stimuler votre passion pour le web.",
-  }
+  title: "À propos | Apprendre Le Web",
+  description:
+    "Explorez le monde du développement web avec des articles captivants et instructifs sur ApprendreLeWeb. Plongez dans des sujets variés, de la programmation aux dernières tendances, pour enrichir vos connaissances et stimuler votre passion pour le web.",
+};
 
 const Page = () => {
-    return (    
+  return (
     <main className={styles.main}>
-        <Section title={"Qui suis-je ?"} content={"Je suis un jeune développeur et designer web, passionné par l'informatique et la création numérique. Mon parcours a débuté avec des études en informatique, où j'ai acquis des compétences solides en développement front-end, notamment en React. En parallèle, j'ai exploré le développement back-end avec Node.js, élargissant ainsi ma gamme de compétences techniques."} />
-        <Section title={"Apprendre Le Web ?"} content={"Je m'interroge fréquemment sur les sujets que je rencontre en développement informatique. C'est pourquoi j'ai pris la décision de créer ce site. Mon objectif est de regrouper toutes ces questions et d'y apporter des réponses à travers des articles. J'espère que ce site sera une ressource précieuse pour toutes les personnes qui se posent les mêmes questions que moi, et qu'il contribuera à éclairer certains des défis que nous rencontrons en développement.\nAu-delà de répondre aux interrogations qui surgissent dans le domaine du développement, ce site représente également ma passion pour le partage de connaissances. J'ai toujours cru en l'importance de la communauté des développeurs, et je souhaite contribuer à son renforcement. En mettant à disposition des articles et des ressources, j'espère créer un espace où les développeurs, qu'ils soient novices ou expérimentés, pourront trouver des réponses, des astuces et des conseils pour progresser dans leur parcours professionnel. Mon aspiration est que ce site devienne un lieu d'apprentissage mutuel et de croissance collective au sein de la communauté du développement."} />
+      <Section
+        title={"Qui suis-je ?"}
+        content={
+          "Je suis un jeune développeur et designer web, passionné par l'informatique et la création numérique. Mon parcours a débuté avec des études en informatique, où j'ai acquis des compétences solides en développement front-end, notamment en React. En parallèle, j'ai exploré le développement back-end avec Node.js, élargissant ainsi ma gamme de compétences techniques."
+        }
+      />
+      <Section
+        title={"Apprendre Le Web ?"}
+        content={
+          "Je m'interroge fréquemment sur les sujets que je rencontre en développement informatique. C'est pourquoi j'ai pris la décision de créer ce site. Mon objectif est de regrouper toutes ces questions et d'y apporter des réponses à travers des articles. J'espère que ce site sera une ressource précieuse pour toutes les personnes qui se posent les mêmes questions que moi, et qu'il contribuera à éclairer certains des défis que nous rencontrons en développement.\nAu-delà de répondre aux interrogations qui surgissent dans le domaine du développement, ce site représente également ma passion pour le partage de connaissances. J'ai toujours cru en l'importance de la communauté des développeurs, et je souhaite contribuer à son renforcement. En mettant à disposition des articles et des ressources, j'espère créer un espace où les développeurs, qu'ils soient novices ou expérimentés, pourront trouver des réponses, des astuces et des conseils pour progresser dans leur parcours professionnel. Mon aspiration est que ce site devienne un lieu d'apprentissage mutuel et de croissance collective au sein de la communauté du développement."
+        }
+      />
+      <Section
+        title={"Où me retrouver ?"}
+        content={
+          "Mes contacts directs sont disponibles sur mon site <a href=\"ayoub-echcharrat.fr\">ayoub-echcharrat.fr</a> ! Vous pouvez m'ajouter sur Linkedin et également voir mes projets sur GitHub. N'hésitez pas à me contacter, nous pourions discuter de vos projets autour d'un chocolat chaud."
+        }
+      />
     </main>
-    )
-}
+  );
+};
 
-export default Page
+export default Page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,76 @@
-import styles from './page.module.css'
+import styles from "./page.module.css";
 
-import LastArticles from '../components/lastArticles/LastArticles'
+import LastArticles from "../components/lastArticles/LastArticles";
 import { Suspense } from "react";
-import Banner from '../components/banner/Banner'
-import Button from '@/components/button/Button.jsx'
-import Image from 'next/image';
+import Banner from "../components/banner/Banner";
+import Button from "@/components/button/Button.jsx";
+import Image from "next/image";
 
 export const metadata = {
-  title: 'Accueil | Apprendre Le Web',
-  description: "Explorez le monde du développement web avec des articles captivants et instructifs sur ApprendreLeWeb. Plongez dans des sujets variés, de la programmation aux dernières tendances, pour enrichir vos connaissances et stimuler votre passion pour le web.",
-}
+  title: "Accueil | Apprendre Le Web",
+  description:
+    "Explorez le monde du développement web avec des articles captivants et instructifs sur ApprendreLeWeb. Plongez dans des sujets variés, de la programmation aux dernières tendances, pour enrichir vos connaissances et stimuler votre passion pour le web.",
+};
 
 export default function Page() {
   return (
     <div>
       <main className={styles.home}>
-        <Banner/>
-        <Suspense fallback={<div className={styles.loadingContainer}><div className={styles.loadingEffect}></div></div>}>
+        <Banner />
+        <Suspense
+          fallback={
+            <div className={styles.loadingContainer}>
+              <div className={styles.loadingEffect}></div>
+            </div>
+          }
+        >
           <div className={styles.presentation}>
             <div className={styles.partLeft}>
-              <h1 className={styles.titlePresentation}>Apprenons le Web ensemble: <br/>
-              Le monde du Développement d&apos;Application Web</h1> 
-              <h3 className={styles.sublinePresentation}>Bienvenue sur mon blog ! Je m&apos;appelle Ayoub, je suis un développeur Front-End spécialisé React.</h3>
-              <p className={styles.paragraph}>En tant que développeur, j&apos;ai consacré des années à créer des interfaces conviviales et à donner vie aux applications web. Doté d&apos;un sens aigu du détail, je suis constamment à l&apos;affût des dernières tendances en développement Front-End, et je suis ravi de partager mes connaissances et mon expertise avec vous.</p>
-              <p className={styles.paragraph}>Sur ce blog, j&apos;aborde divers sujets liés au développement de logiciels, en mettant l&apos;accent sur JavaScript, React, Next.Js, Jest et Node.Js. De temps en temps, j&apos;écris également sur la productivité des développeurs.</p>
-              <Button link='/articles' content="C'est parti" />
+              <h1 className={styles.titlePresentation}>
+                Apprenons le Web ensemble
+              </h1>
+              <h3 className={styles.sublinePresentation}>
+                Bienvenue sur mon blog ! Je m&apos;appelle Ayoub, je suis un
+                développeur fullstack spécialisé en React et Java.
+              </h3>
+              <p className={styles.paragraph}>
+                En tant que développeur, j&apos;ai consacré des années à créer
+                des interfaces conviviales et à donner vie aux applications web.
+                Doté d&apos;un sens aigu du détail, je suis constamment à
+                l&apos;affût des dernières tendances en développement, et je
+                suis ravi de partager mes connaissances et mon expertise avec
+                vous.
+              </p>
+              <p className={styles.paragraph}>
+                Sur ce blog, j&apos;aborde divers sujets liés au développement
+                de logiciels, en mettant l&apos;accent sur JavaScript, React,
+                NextJs, et NodeJs. De temps en temps, j&apos;écris également sur
+                la productivité des développeurs.
+              </p>
+              <Button link="/articles" content="C'est parti" />
             </div>
             <div className={styles.partRight}>
-              <Image quality={100} className={styles.illuPresentation} src="/illu-computer.png" width={700} height={700} alt="" />
-              <Image quality={0} className={styles.illuShadowPresentation} src="/illu-computer.png" width={500} height={500} alt="" />
+              <Image
+                quality={100}
+                className={styles.illuPresentation}
+                src="/illu-computer.png"
+                width={700}
+                height={700}
+                alt=""
+              />
+              <Image
+                quality={0}
+                className={styles.illuShadowPresentation}
+                src="/illu-computer.png"
+                width={500}
+                height={500}
+                alt=""
+              />
             </div>
           </div>
-          <LastArticles/>  
-      </Suspense>
+          <LastArticles />
+        </Suspense>
       </main>
     </div>
-  )
+  );
 }
