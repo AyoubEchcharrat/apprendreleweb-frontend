d72e68223c6f342cf1ec676bcefee932ce7ba146
Ayoub E <ayoub.ech-charrat@hotmail.fr>
2025-02-20 10:42:54 +0100

    only 20 tags displayed and randomly

--- src/components/getAllTags/GetAllTags.jsx
+++ src/components/getAllTags/GetAllTags.jsx
@@ -2,32 +2,40 @@ import "@/components/getAllTags/getAllTags.css";
 import Link from "next/link";
 
 async function getArticles() {
-	const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/articles/?timestamp=${Date.now()}`, {
-		cache: "no-store",
-	});
-	if (!res.ok) {
-		throw new Error("Failed to fetch data");
-	}
-	return res.json();
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/articles/?timestamp=${Date.now()}`,
+    {
+      cache: "no-store",
+    }
+  );
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
 }
 
 export default async function GetAllTags() {
-	const articles = await getArticles();
-	const listOfTags = new Set();
+  const articles = await getArticles();
+  const listOfTags = new Set();
+  const limit = 20;
 
-	articles.forEach((article) => {
-		article.tags.forEach((articleTags) => {
-			listOfTags.add(articleTags.toLowerCase());
-		});
-	});
-	const uniqueTags = [...listOfTags];
-	return (
-		<div className="tags-container">
-			{uniqueTags.map((tag, index) => (
-				<Link key={`tag-${index}`} href={`/tags/${tag}`}>
-					<div className="tag-bubble">{tag}</div>
-				</Link>
-			))}
-		</div>
-	);
+  articles.forEach((article) => {
+    article.tags.forEach((tag) => listOfTags.add(tag.toLowerCase()));
+  });
+
+  const tagsArray = Array.from(listOfTags);
+
+  const shuffledTags = tagsArray.sort(() => Math.random() - 0.5);
+
+  const randomTags = shuffledTags.slice(0, limit);
+
+  return (
+    <div className="tags-container">
+      {randomTags.map((tag, index) => (
+        <Link key={`tag-${index}`} href={`/tags/${tag}`}>
+          <div className="tag-bubble">{tag}</div>
+        </Link>
+      ))}
+    </div>
+  );
 }
