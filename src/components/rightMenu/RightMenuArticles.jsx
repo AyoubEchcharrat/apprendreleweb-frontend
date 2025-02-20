7a738d8607274394075c3449c5a0c2c3cedc8dcf
Ayoub E <ayoub.ech-charrat@hotmail.fr>
2025-02-20 10:57:51 +0100

    display content tablet format

--- src/components/rightMenu/RightMenuArticles.jsx
+++ src/components/rightMenu/RightMenuArticles.jsx
@@ -1,15 +1,16 @@
-import GetAllTags from '@/components/getAllTags/GetAllTags'
-import RandomArticle from '@/components/randomArticle/RandomArticle'
-import './rightMenu.css'
+import GetAllTags from "@/components/getAllTags/GetAllTags";
+import RandomArticle from "@/components/randomArticle/RandomArticle";
+import "./rightMenu.css";
 
-export default function RightMenuArticles({articles}) {
-
-    return (
-        <div className='sticky'>
-          <p className='title_rightMenu'>Table des <span style={{color:'#6d6dec'}}>tags</span></p>
-          <GetAllTags/>
-            <p className='title_rightMenu'>Article populaire</p>
-            <RandomArticle articles={articles}/>
-        </div>
-    )
+export default function RightMenuArticles({ articles }) {
+  return (
+    <div className="sticky">
+      <p className="title_rightMenu">
+        Table des <span style={{ color: "#6d6dec" }}>tags</span>
+      </p>
+      <GetAllTags />
+      <p className="title_rightMenu">Article populaire</p>
+      <RandomArticle articles={articles} />
+    </div>
+  );
 }
